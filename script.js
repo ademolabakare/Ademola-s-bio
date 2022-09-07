8ce0b38dd35452ac22adc38ff11505bd1af72002
ademolabakare <106655720+ademolabakare@users.noreply.github.com>
2022-09-07 02:04:01 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".col-4").hover(function(){
+	$(this).addClass("highlight");
+		},function(){
+	$(this).removeClass("highlight");
+});
+
+$(".fa").hover(function(){
+	$(this).addClass("icon");
+		},function(){
+	$(this).removeClass("icon");
+});
